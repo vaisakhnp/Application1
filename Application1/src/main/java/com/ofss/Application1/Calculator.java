cc87a0cacc55e72fa342b308982166a2bea75d1e
VNP19 <vaisakh.np@oracle.com>
2019-04-16 12:16:44 +0530

    Branch1 commit operation Print message change by vaisakh n p

--- Application1/src/main/java/com/ofss/Application1/Calculator.java
+++ Application1/src/main/java/com/ofss/Application1/Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
 	  public static void main( String[] args )
 	    {
 	        System.out.println( "Hello World123" );
+	        System.out.println( "Hello World in Branch1" );
 	    }
 
 }
